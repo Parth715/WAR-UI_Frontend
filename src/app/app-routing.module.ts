85f7d15a2ea6778d184f2a65d2fec1b380e458ab
Parth Patel <parth2002715@gmail.com>
2021-12-14 19:02:29 -0500

    Routing was added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlayerComponent } from './player/player.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
-  {path: "", redirectTo:"/welcome", pathMatch: "full"},
-  {path: "Players", component: PlayerComponent}
+  {path: "", redirectTo:"/Welcome", pathMatch: "full"},
+  {path: "/Players", component: PlayerComponent},
+  {path: "/Welcome", component: WelcomeComponent}
 ];
 
 @NgModule({
