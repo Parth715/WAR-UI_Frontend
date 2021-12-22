4caacd1073ead6ed0328b93610f717236e228749
Parth Patel <parth2002715@gmail.com>
2021-12-22 17:12:30 -0500

    incorporate card photopath and size on screen

--- src/app/play/card.ts
+++ src/app/play/card.ts
@@ -2,9 +2,11 @@ export class Card {
     id: number = 0
     cardnumber: number = 0
     face: string = ""
-    constructor(id: number, cardnumber: number, face: string){
+    photo: string = ""
+    constructor(id: number, cardnumber: number, face: string, photo: string){
         this.id = id,
         this.cardnumber = cardnumber,
-        this.face = face
+        this.face = face,
+        this.photo = photo
     }
 }
