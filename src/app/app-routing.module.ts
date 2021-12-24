61750b905378fd908a93021abe434215b1e60c51
Parth Patel <parth2002715@gmail.com>
2021-12-23 20:07:22 -0500

    Made a screen where you can select your player

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlayComponent } from './play/play.component';
 import { PlayerComponent } from './player/player.component';
+import { SelectplayerComponent } from './selectplayer/selectplayer.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
   {path: "player", component: PlayerComponent},
   {path: "welcome", component: WelcomeComponent},
   {path: "play", component: PlayComponent},
+  {path: "selectplayer", component: SelectplayerComponent},
   {path: "**", component: WelcomeComponent}
 ];
 
